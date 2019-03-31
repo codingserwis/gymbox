a4dbc73f3ed7a48ec33b1c58efecffe3a5e57c0e
CodingSerwis <maciej.lehrer@codingserwis.pl>
2019-03-31 14:10:50 +0200

    header and footer

---
+++ footer.php
@@ -0,0 +1,50 @@
+<footer class="footer py-4 bg-black">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 mb-4">
+				<ul class="footer-nav">
+					<li>
+						<a href="#start">start</a>
+					</li>
+					<li>
+						<a href="#grafik">grafik</a>
+					</li>
+					<li>
+						<a href="#cennik">cennik</a>
+					</li>
+					<li>
+						<a href="#kontakt">kontakt</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6 fc-white text-center text-md-right fs-12 fw-300">
+				Copyrights &copy; Gymbox <?php echo date('Y'); ?>
+			</div>
+			<div class="col-12 col-md-6 text-center text-md-left fc-white fs-12 fw-300 mt-3 mt-md-0">
+				Projekt i wykonanie: <a href="http://codingserwis.pl" class="fc-white fs-12 fw-300">CodingSerwis</a>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- <footer class="footer bg_black">
+	</footer>
+	<div id="coockie_info" class="cookie_main">
+		<p class="cookie_text">Strona uzywa plików cookie. <a id="cookie_close_btn" class="btn btn-info btn-xs">Akceptuję</a></button></p>
+	</div>
+	<div class="scroll_top hidden-xs">
+		<a href="#top" style="padding: 12px;">
+			<span class="glyphicon glyphicon-menu-up font_white" aria-hidden="true"></span>
+		</a>
+	</div>
+	<script type="text/javascript" src="./assets/js/animations.js"></script>
+		<script type="text/javascript" src="./assets/js/cookie.js"></script>
+		<script type="text/javascript" src="./assets/js/scheodule_nav.js"></script>
+		<script type="text/javascript" src="./assets/js/scrolling.js"></script>
+		<script type="text/javascript" src="./assets/js/sh_mod.js"></script>
+		<script type="text/javascript" src="./assets/js/wod_navi.js"></script> -->
+    <!-- <script src="./assets/js/app.js"></script> -->
+    <?php wp_footer( ); ?>
+</body>
+</html>
