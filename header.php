076f94c3995b762fd03710329f98ebbd4d98c85b
CodingSerwis <maciej.lehrer@codingserwis.pl>
2019-04-14 13:53:49 +0200

    social media

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="<?php bloginfo( 'language' ); ?>">
 	<head>
-		<title>Gymbox Opole - Trenujemy Crossfit</title>
-		<meta charset="utf-8">
-		<meta name="description" content="Gymbox Opole - trenujemy CrossFit. Tylko w naszym boxie FitBox, Cross Lejdis i Cross Student, orazy treningi CrossFit - WOD, CrossFit Basic.">
+		<title><?php bloginfo( 'name' ); ?></title>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 		<meta name="keywords" content="Gymbox, Gymbox Opole, Gym Box, gym box opole, Crossfit, Cross fit, Fitness, Trening, Trening personalny, Cross fit box, wod, crossfit fitness, WOD, FitBox, fit box, Cross Lejdis, CrossLejdis, crossfit budowlanych, crossfit opole budowlanych, fitness opole, siłownia opole">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head( ); ?>
